502ebff31d4544549e32b24c9c513634a1480101
Lisitsin <113295347+lisenok-lisitsin@users.noreply.github.com>
2023-03-25 01:00:17 +0300

    Add files via upload

---
+++ MyProjWalk/Project Walking/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Project_Walking
+{
+    partial class MainVoid
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainVoid));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picboxMain = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cacti = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cacti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 558);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1264, 123);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Statistics";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // picboxMain
+            // 
+            this.picboxMain.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picboxMain.BackColor = System.Drawing.Color.Transparent;
+            this.picboxMain.Image = ((System.Drawing.Image)(resources.GetObject("picboxMain.Image")));
+            this.picboxMain.Location = new System.Drawing.Point(620, 520);
+            this.picboxMain.Name = "picboxMain";
+            this.picboxMain.Size = new System.Drawing.Size(32, 32);
+            this.picboxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picboxMain.TabIndex = 1;
+            this.picboxMain.TabStop = false;
+            this.picboxMain.LocationChanged += new System.EventHandler(this.picboxMain_LocationChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "mk2sprite.png");
+            this.imageList1.Images.SetKeyName(1, "mk2spriteWL2.png");
+            this.imageList1.Images.SetKeyName(2, "mk2spriteWL.png");
+            this.imageList1.Images.SetKeyName(3, "mk2spriteWR2.png");
+            this.imageList1.Images.SetKeyName(4, "mk2spriteWR.png");
+            this.imageList1.Images.SetKeyName(5, "mk2spriteWLU2.png");
+            this.imageList1.Images.SetKeyName(6, "mk2spriteWLU.png");
+            this.imageList1.Images.SetKeyName(7, "mk2spriteWLD2.png");
+            this.imageList1.Images.SetKeyName(8, "mk2spriteWLD.png");
+            this.imageList1.Images.SetKeyName(9, "mk2spriteUP2.png");
+            this.imageList1.Images.SetKeyName(10, "mk2spriteUP.png");
+            this.imageList1.Images.SetKeyName(11, "mk2spriteDOWN2.png");
+            this.imageList1.Images.SetKeyName(12, "mk2spriteDOWN.png");
+            // 
+            // cacti
+            // 
+            this.cacti.BackColor = System.Drawing.Color.Transparent;
+            this.cacti.Image = ((System.Drawing.Image)(resources.GetObject("cacti.Image")));
+            this.cacti.Location = new System.Drawing.Point(1065, 219);
+            this.cacti.Name = "cacti";
+            this.cacti.Size = new System.Drawing.Size(32, 32);
+            this.cacti.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.cacti.TabIndex = 2;
+            this.cacti.TabStop = false;
+            // 
+            // MainVoid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.picboxMain);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cacti);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "MainVoid";
+            this.Text = "Walking One";
+            this.Load += new System.EventHandler(this.MainVoid_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainVoid_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainVoid_KeyPress);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.MainVoid_PreviewKeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cacti)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox picboxMain;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox cacti;
+    }
+}
+
---
+++ MyProjWalk/Project Walking/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Walking
+{
+    public partial class MainVoid : Form
+    {
+        public MainVoid()
+        {
+            InitializeComponent();
+        }
+        private void DebugOne_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void MainVoid_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        int lefcounter = 1;
+        int rigcounter = 3;
+        //int upcounter = 5;
+        //int downcounter = 7;
+        int uprealcount = 9;
+        int downrealcount = 11;
+
+        class MediaPlayer
+        {
+            System.Media.SoundPlayer soundPlayer;
+            public MediaPlayer(byte[] buffer)
+            {
+                MemoryStream memoryStream = new MemoryStream(buffer, true);
+                soundPlayer = new System.Media.SoundPlayer(memoryStream);
+            }
+            public void Play() { soundPlayer.Play(); }
+            public void Play(byte[] buffer)
+            {
+                soundPlayer.Stream.Seek(0, SeekOrigin.Begin);
+                soundPlayer.Stream.Write(buffer, 0, buffer.Length);
+                soundPlayer.Play();
+            }
+        }
+
+        private void MainVoid_KeyDown(object sender, KeyEventArgs e)
+
+        {
+            int x = picboxMain.Location.X;
+            int y = picboxMain.Location.Y;
+            int xc = cacti.Location.X;
+            int yc = cacti.Location.Y;
+
+            if (e.KeyCode == Keys.F)
+            {
+                if ((x <= 1100 && x >= 1030)&&(240 >= y && y >= 210))
+                {
+                    string cacti = @"..\..\Resources\samcacti.wav";
+                    List<byte> soundBytes = new List<byte>(File.ReadAllBytes(cacti));
+                    MediaPlayer mPlayer = new MediaPlayer(soundBytes.ToArray());
+                    mPlayer.Play();
+                }
+                else
+                {
+                    string nothing = @"..\..\Resources\samtalk.wav";
+                    List<byte> soundBytes = new List<byte>(File.ReadAllBytes(nothing));
+                    MediaPlayer mPlayer = new MediaPlayer(soundBytes.ToArray());
+                    mPlayer.Play();
+                }
+            }
+
+            if (e.KeyCode == Keys.F1)
+            {
+                string heyy = @"..\..\Resources\samdoesntlike.wav";
+                List<byte> soundBytes = new List<byte>(File.ReadAllBytes(heyy));
+                MediaPlayer mPlayer = new MediaPlayer(soundBytes.ToArray());
+                mPlayer.Play();
+                MessageBox.Show("Hey! It's simple tutorial: \nWalk using arrows and you'll be just fine");
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                x += 10;
+
+                if (rigcounter < 5)
+                {
+                    picboxMain.Image = imageList1.Images[rigcounter];
+                    rigcounter++;
+                }
+                else
+                {
+                    rigcounter = 3;
+                    picboxMain.Image = imageList1.Images[3];
+                    picboxMain.Refresh();
+                    rigcounter++;
+                }
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                x -= 10;
+
+                if (lefcounter < 3)
+                {
+                    picboxMain.Image = imageList1.Images[lefcounter];
+                    lefcounter++;
+                }
+                else
+                {
+                    lefcounter = 1;
+                    picboxMain.Image = imageList1.Images[1];
+                    picboxMain.Refresh();
+                    lefcounter++;
+                }
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                y -= 10;
+
+                //if (upcounter < 7)
+                //{
+                //    picboxMain.Image = imageList1.Images[upcounter];
+                //    upcounter++;
+                //}
+                //else
+                //{
+                //    upcounter = 5;
+                //    picboxMain.Image = imageList1.Images[5];
+                //    picboxMain.Refresh();
+                //    upcounter++;
+                //}
+
+                if (uprealcount < 11)
+                {
+                    picboxMain.Image = imageList1.Images[uprealcount];
+                    uprealcount++;
+                }
+                else
+                {
+                    uprealcount = 9;
+                    picboxMain.Image = imageList1.Images[uprealcount];
+                    picboxMain.Refresh();
+                    uprealcount++;
+                }
+
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                y += 10;
+
+                //if (downcounter < 9)
+                //{
+                //    picboxMain.Image = imageList1.Images[downcounter];
+                //    downcounter++;
+                //}
+                //else
+                //{
+                //    downcounter = 7;
+                //    picboxMain.Image = imageList1.Images[7];
+                //    picboxMain.Refresh();
+                //    downcounter++;
+                //}
+
+                if (downrealcount < 13)
+                {
+                    picboxMain.Image = imageList1.Images[downrealcount];
+                    downrealcount++;
+                }
+                else
+                {
+                    downrealcount = 11;
+                    picboxMain.Image = imageList1.Images[downrealcount];
+                    picboxMain.Refresh();
+                    downrealcount++;
+                }
+            }
+
+            picboxMain.Location = new Point(x, y);
+            
+            if (x < 0)
+            {
+                picboxMain.Location = new Point(1240, y);
+            }
+            if (x > 1240)
+            {
+                picboxMain.Location = new Point(0, y);
+            }
+            if (y < 0)
+            {
+                picboxMain.Location = new Point(x, 520);
+            }
+            if (y > 520)
+            {
+                picboxMain.Location = new Point(x, 0);
+            }
+            
+        }
+
+
+        private void picboxMain_LocationChanged(object sender, EventArgs e)
+        {
+            int x = picboxMain.Location.X;
+            int y = picboxMain.Location.Y;
+            label1.Text = "X is " + Convert.ToString(x) + " " + "Y is " + Convert.ToString(y);
+        }
+        private void MainVoid_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            int x = picboxMain.Location.X;
+            int y = picboxMain.Location.Y;
+            label1.Text = "X is " + Convert.ToString(x) + " " + "Y is " + Convert.ToString(y);
+            string launch = @"..\..\Resources\samhello.wav";
+            List<byte> soundBytes = new List<byte>(File.ReadAllBytes(launch));
+            MediaPlayer mPlayer = new MediaPlayer(soundBytes.ToArray());
+            mPlayer.Play();
+        }
+
+        private void MainVoid_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            
+        }
+    }
+}
